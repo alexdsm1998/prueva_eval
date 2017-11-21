1e273479c70a07b84bf73781918d51319c809bbd
aldema <alexdelsaz@gmail.com>
2017-11-21 10:46:08 +0100

    hasta el punto 2 el 3 ya no me salia ;(

---
+++ pe.js
@@ -0,0 +1,53 @@
+var intentos=prompt('cuantos intentos desea tener');
+var array=[];
+var boolean=true;
+var aciertos=0;
+var palabras=['dani','buenas','como','estas','hoy'];
+var aleat=Math.floor(Math.random()*palabras.length);
+var adivinar=palabras[aleat];
+var fallos=0;
+var array=[];
+var n = adivinar.length;
+console.log(palabras);
+console.log('numero de fallos',fallos);
+for (var i = 0; i < n; i++) {
+  array[i]='0'*i;
+}
+if(intentos>5 && intentos<=10){
+while (fallos<intentos && aciertos<adivinar.length) {
+boolean=true;
+  var letrausu =prompt("introduce una  letra");
+  while (boolean) {
+      if (isNaN(letrausu)) {
+        boolean=false;
+      } else {
+          var letrausu = prompt("tienes que introducir una letra");
+          boolean=true;
+          fallos++;
+        }
+      }
+
+  var acertado=false;
+  for (var i = 0; i < n; i++) {
+    if (adivinar.charAt(i)==letrausu) {
+      array[i]=adivinar.charAt(i);
+      console.log(array);
+      acertado=true;
+      aciertos++;
+    }
+  }
+  if (acertado==true) {
+    alert("has acertado");
+    console.log('numero de fallos',fallos);
+  } else {
+    alert("letra no encontrada");
+    fallos++;
+    console.log('numero de fallos',fallos);
+  }
+  if (letrausu=='fin' || letrausu=='FIN') {
+    fallos=intentos;
+    alert('el juego ha terminado');
+  }
+ }
+}
+console.log(array);
